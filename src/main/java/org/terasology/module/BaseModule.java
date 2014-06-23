8649db44a22a852f8cf57364f693b95add113368
Immortius <immortius@gmail.com>
2014-06-23 18:48:12 +1000

    Added support for cached reflections in modules.

--- src/main/java/org/terasology/module/BaseModule.java
+++ src/main/java/org/terasology/module/BaseModule.java
@@ -23,9 +23,17 @@ import org.reflections.scanners.SubTypesScanner;
 import org.reflections.scanners.TypeAnnotationsScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.naming.Name;
 import org.terasology.naming.Version;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Objects;
@@ -36,6 +44,9 @@ import java.util.Objects;
  * @author Immortius
  */
 public abstract class BaseModule implements Module {
+    private static final String REFLECTIONS_CACHE_FILE = "reflections.cache";
+    private static final Logger logger = LoggerFactory.getLogger(BaseModule.class);
+
     protected ImmutableList<Path> paths;
     protected ModuleMetadata metadata;
 
@@ -74,13 +85,49 @@ public abstract class BaseModule implements Module {
     public Reflections getReflectionsFragment() {
         Preconditions.checkState(isCodeModule(), "Cannot get reflections fragment for non-code module");
         if (reflectionsFragment == null) {
-            reflectionsFragment = new ConfigurationBuilder()
-                    .addUrls(getClasspaths())
-                    .setScanners(new TypeAnnotationsScanner(), new SubTypesScanner())
-                    .addClassLoader(ClasspathHelper.staticClassLoader())
-                    .build();
+            for (Path path : paths) {
+                if (Files.isDirectory(path) && Files.isRegularFile(path.resolve(REFLECTIONS_CACHE_FILE))) {
+                    collectCacheFromPath(path);
+                } else if (Files.isRegularFile(path)) {
+                    collectCacheFromArchive(path);
+                }
+            }
+            if (reflectionsFragment == null) {
+                scanForReflections();
+            }
         }
         return reflectionsFragment;
+    }
+
+    private void scanForReflections() {
+        reflectionsFragment = new ConfigurationBuilder()
+                .addUrls(getClasspaths())
+                .setScanners(new TypeAnnotationsScanner(), new SubTypesScanner())
+                .addClassLoader(ClasspathHelper.staticClassLoader())
+                .build();
+    }
+
+    private void collectCacheFromArchive(Path path) {
+        try (FileSystem archive = FileSystems.newFileSystem(path, null)) {
+            Path cachePath = archive.getPath(REFLECTIONS_CACHE_FILE);
+            if (Files.isRegularFile(cachePath)) {
+                if (reflectionsFragment == null) {
+                    reflectionsFragment = new ConfigurationBuilder().addClassLoader(ClasspathHelper.staticClassLoader()).build();
+                }
+                try (InputStream stream = new BufferedInputStream(Files.newInputStream(cachePath))) {
+                    reflectionsFragment.collect(stream);
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Failure attempting to read reflections cache from {}", this, e);
+        }
+    }
+
+    private void collectCacheFromPath(Path path) {
+        if (reflectionsFragment == null) {
+            reflectionsFragment = new ConfigurationBuilder().addClassLoader(ClasspathHelper.staticClassLoader()).build();
+        }
+        reflectionsFragment.collect(path.resolve(REFLECTIONS_CACHE_FILE).toFile());
     }
 
     @Override
