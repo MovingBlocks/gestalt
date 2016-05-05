5666bcfddbb77bc759634425c66c49d7ea6249ab
Martin Steiger <git@martin-steiger.de>
2016-05-05 10:52:54 +0200

    Completed JavaDoc for AssetManager

--- gestalt-asset-core/src/main/java/org/terasology/assets/management/AssetManager.java
+++ gestalt-asset-core/src/main/java/org/terasology/assets/management/AssetManager.java
@@ -34,8 +34,8 @@ import java.util.Set;
 /**
  * AssetManager provides an simplified interface for working with assets across multiple asset types.
  * <p>
- * To do this it uses an AssetManager to obtain the AssetTypes relating to an Asset class of interest, and delegates down to them for actions such
- * as obtaining and reloading assets.
+ * To do this it uses an {@link AssetTypeManager} to obtain the AssetTypes relating to an Asset
+ * class of interest, and delegates down to them for actions such as obtaining and reloading assets.
  * </p>
  *
  * @author Immortius
@@ -46,6 +46,9 @@ public final class AssetManager {
 
     private final AssetTypeManager assetTypeManager;
 
+    /**
+     * @param assetTypeManager the asset type manager that will be used
+     */
     public AssetManager(AssetTypeManager assetTypeManager) {
         this.assetTypeManager = assetTypeManager;
     }
