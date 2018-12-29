6deaafeccd04a1a8c7fbaca7eede63a0914c8135
immortius@gmail.com <immortius@gmail.com>
2018-12-29 16:48:19 +1100

    Changed min api version to 24 to support Optional (for now at least) Updated gradle for gestalt-module and its test modules.

--- gestalt-android-testbed/src/main/java/org/terasology/gestalt/android/testbed/GestaltTestActivity.java
+++ gestalt-android-testbed/src/main/java/org/terasology/gestalt/android/testbed/GestaltTestActivity.java
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import org.terasology.util.collection.KahnSorter;
-
 public class GestaltTestActivity extends AppCompatActivity {
 
     @Override
@@ -13,18 +11,7 @@ public class GestaltTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestalt_test);
         TextView text = findViewById(R.id.textDisplay);
-
-        KahnSorter<String> sorter = new KahnSorter<>();
-        sorter.addNode("E");
-        sorter.addNode("A");
-        sorter.addNode("C");
-        sorter.addNode("B");
-        sorter.addNode("D");
-        sorter.addEdge("B", "A");
-        sorter.addEdge("C", "B");
-        sorter.addEdge("D", "C");
-        sorter.addEdge("E", "D");
-
-        text.setText("Test: " + sorter.sort());
+        //text.setText("Test: " + map.valueFor(Locale.JAPANESE));
     }
+
 }
