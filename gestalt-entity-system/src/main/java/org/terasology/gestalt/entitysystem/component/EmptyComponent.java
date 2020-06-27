74903156909ef5d336dc278108e4719b70ddc7da
immortius@gmail.com <immortius@gmail.com>
2020-06-28 00:59:40 +1000

    Fix for inclusion of the extra repository for java-semver Some documentation changes for EmptyComponent

--- gestalt-entity-system/src/main/java/org/terasology/gestalt/entitysystem/component/EmptyComponent.java
+++ gestalt-entity-system/src/main/java/org/terasology/gestalt/entitysystem/component/EmptyComponent.java
@@ -1,11 +1,12 @@
 package org.terasology.gestalt.entitysystem.component;
 
 /**
- * An abstract component to be extended if a component that has no information to copy, like components that are flags
- * for properties.
+ * An abstract component that can be extended from for any component with no mutable elements - so components with no attributes. This avoids needing to implement the copy method.
+ * EmptyComponent may be used for flagging behavior where no configuration is required. I would suggest this should not come up hugely often, and if you are finding you have a lot
+ * of empty components you might consider whether they would be better handled by another component having boolean attributes.
  * <p>
- * This should only be used for components that contain no information. If this is used for a component that does
- * contain data, that data could be lost.
+ * Note if this is used for a component with attributes and the copy method is not overwritten then the component will not behave as desired - effectively the entity system will
+ * not retain any values given to the attributes.
  */
 public abstract class EmptyComponent implements Component<EmptyComponent> {
 
