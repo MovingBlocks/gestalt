463b0fcc1acb30995f6e663fbf4c2e43984727e2
DarkWeird <foxflameless@gmail.com>
2021-03-15 16:09:26 +0300

    fix(gestalt-DI): `@Inherited` cannot works with annotation. handle this case.

--- gestalt-inject-java/src/main/java/org/terasology/gestalt/annotation/processing/ClassIndexProcessor.java
+++ gestalt-inject-java/src/main/java/org/terasology/gestalt/annotation/processing/ClassIndexProcessor.java
@@ -1,5 +1,6 @@
 package org.terasology.gestalt.annotation.processing;
 
+import com.google.common.collect.Queues;
 import org.terasology.context.annotation.Index;
 import org.terasology.context.annotation.IndexInherited;
 
@@ -11,10 +12,13 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
+import javax.tools.Diagnostic;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Queue;
 import java.util.Set;
 
 public class ClassIndexProcessor extends AbstractProcessor {
@@ -38,37 +42,80 @@ public class ClassIndexProcessor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (TypeElement annotation : annotations) {
             // Annotation Index
-            if (elementUtility.hasStereotype(annotation, Collections.singletonList(Index.class.getName()))) {
-                for (Element type : roundEnv.getElementsAnnotatedWith(annotation)) {
-                    if (type.getKind() == ElementKind.CLASS) {
-                        annotationTypeWriter.writeAnnotation(annotation.getQualifiedName().toString(), type.asType().toString());
-                    } else if (type.getKind() == ElementKind.PACKAGE) {
-                        PackageElement packageType = (PackageElement) type;
-                        for (Element enclosedElement : packageType.getEnclosedElements()) {
-                            annotationTypeWriter.writeAnnotation(annotation.getQualifiedName().toString(), enclosedElement.asType().toString());
-                        }
-                    }
-                }
-            }
-            if (Arrays.asList(Index.class.getName(), IndexInherited.class.getName()).contains(annotation)) {
-                for (Element type : roundEnv.getElementsAnnotatedWith(annotation)) {
-                    if (type.getKind() == ElementKind.CLASS) {
-                        for (TypeMirror tm : ((TypeElement) type).getInterfaces()) {
-                            subtypesTypeWriter.writeSubType(tm.toString(), type.asType().toString());
-                        }
-                    }
-                }
-            }
+            processAnnotationIndex(roundEnv, annotation);
+            // Subtypes index. classes under index.
+            processSubtypeIndexByDirectMarked(roundEnv, annotation);
         }
+        // Subtypes index. every class, which can have interface with `@IndexInherited` annotation.
+        processSubtypeIndexInReverseWay(roundEnv);
+
         if (roundEnv.processingOver()) {
-            try {
-                annotationTypeWriter.finish();
-                subtypesTypeWriter.finish();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            writeIndexes();
         }
         return false;
+    }
+
+    private void processSubtypeIndexInReverseWay(RoundEnvironment roundEnv) {
+        for (Element type : roundEnv.getRootElements()) {
+            Queue<TypeMirror> supers = Queues.newArrayDeque();
+            if (type.getKind() == ElementKind.CLASS) {
+                supers.addAll(elementUtility.getTypes().directSupertypes(type.asType()));
+                while (!supers.isEmpty()) {
+                    TypeMirror candidate = supers.poll();
+                    if (candidate.getKind() != TypeKind.NONE) {
+                        if (elementUtility.hasStereotype(elementUtility.getTypes().asElement(candidate),
+                                Collections.singletonList(IndexInherited.class.getName())))
+                            subtypesTypeWriter.writeSubType(elementUtility.getTypes().erasure(candidate).toString(), type.asType().toString());
+                        supers.addAll(elementUtility.getTypes().directSupertypes(candidate));
+                    }
+                }
+            }
+        }
+    }
+
+    private void processSubtypeIndexByDirectMarked(RoundEnvironment roundEnv, TypeElement annotation) {
+        if (Arrays.asList(Index.class.getName(), IndexInherited.class.getName()).contains(annotation.asType().toString())) {
+            for (Element type : roundEnv.getElementsAnnotatedWith(annotation)) {
+                if (type.getKind() == ElementKind.CLASS) {
+                    TypeElement typeElement = (TypeElement) type;
+                    Queue<TypeMirror> supers = Queues.newArrayDeque();
+                    supers.addAll(elementUtility.getTypes().directSupertypes(typeElement.asType()));
+                    while (!supers.isEmpty()) {
+                        TypeMirror candidate = supers.poll();
+                        if (candidate.getKind() != TypeKind.NONE) {
+                            if (elementUtility.hasStereotype(elementUtility.getTypes().asElement(candidate),
+                                    Collections.singletonList(IndexInherited.class.getName())))
+                                subtypesTypeWriter.writeSubType(elementUtility.getTypes().erasure(candidate).toString(), type.asType().toString());
+                            supers.addAll(elementUtility.getTypes().directSupertypes(candidate));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void processAnnotationIndex(RoundEnvironment roundEnv, TypeElement annotation) {
+        if (elementUtility.hasStereotype(annotation, Collections.singletonList(Index.class.getName()))) {
+            for (Element type : roundEnv.getElementsAnnotatedWith(annotation)) {
+                if (type.getKind() == ElementKind.CLASS) {
+                    annotationTypeWriter.writeAnnotation(annotation.getQualifiedName().toString(), type.asType().toString());
+                } else if (type.getKind() == ElementKind.PACKAGE) {
+                    PackageElement packageType = (PackageElement) type;
+                    for (Element enclosedElement : packageType.getEnclosedElements()) {
+                        annotationTypeWriter.writeAnnotation(annotation.getQualifiedName().toString(), enclosedElement.asType().toString());
+                    }
+                }
+            }
+        }
+    }
+
+    private void writeIndexes() {
+        try {
+            annotationTypeWriter.finish();
+            subtypesTypeWriter.finish();
+        } catch (IOException e) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Cannot write indexes: " + e.getMessage());
+        }
     }
 
     @Override
