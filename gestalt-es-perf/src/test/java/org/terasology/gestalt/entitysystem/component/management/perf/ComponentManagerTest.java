c00bd12273c0482f4134cf239f0de4de14b723e8
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-04-15 19:42:00 -0700

    test: upgrade gestalt-es-perf tests to JUnit 5

--- gestalt-es-perf/src/test/java/org/terasology/gestalt/entitysystem/component/management/perf/ComponentManagerTest.java
+++ gestalt-es-perf/src/test/java/org/terasology/gestalt/entitysystem/component/management/perf/ComponentManagerTest.java
@@ -1,23 +1,14 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.gestalt.entitysystem.component.management.perf;
 
-import org.junit.Before;
-import org.junit.Test;
+import modules.test.components.ArrayContainingComponent;
+import modules.test.components.BasicComponent;
+import modules.test.components.Empty;
+import modules.test.components.PublicAttributeComponent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.gestalt.entitysystem.component.management.ComponentManager;
 import org.terasology.gestalt.entitysystem.component.management.ComponentType;
@@ -26,16 +17,11 @@ import org.terasology.gestalt.entitysystem.component.management.PropertyAccessor
 
 import java.util.Optional;
 
-import modules.test.components.ArrayContainingComponent;
-import modules.test.components.BasicComponent;
-import modules.test.components.Empty;
-import modules.test.components.PublicAttributeComponent;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
@@ -44,7 +30,7 @@ public abstract class ComponentManagerTest {
 
     private ComponentManager componentManager;
 
-    @Before
+    @BeforeEach
     public void before() {
         componentManager = new ComponentManager(getComponentTypeFactory());
     }
