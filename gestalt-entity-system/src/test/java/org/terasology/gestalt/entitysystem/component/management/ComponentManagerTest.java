7453844e00e55a272c13da7e24fd35dffb9cbf34
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-04-15 19:25:20 -0700

    test: upgrade gestalt-entity-system tests to JUnit 5

--- gestalt-entity-system/src/test/java/org/terasology/gestalt/entitysystem/component/management/ComponentManagerTest.java
+++ gestalt-entity-system/src/test/java/org/terasology/gestalt/entitysystem/component/management/ComponentManagerTest.java
@@ -1,38 +1,23 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.gestalt.entitysystem.component.management;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.terasology.gestalt.entitysystem.component.Component;
-
-import java.util.Arrays;
-import java.util.Optional;
 
 import modules.test.components.ArrayContainingComponent;
 import modules.test.components.BasicComponent;
 import modules.test.components.Empty;
 import modules.test.components.PublicAttributeComponent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.terasology.gestalt.entitysystem.component.Component;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
@@ -41,7 +26,7 @@ public abstract class ComponentManagerTest {
 
     private ComponentManager componentManager;
 
-    @Before
+    @BeforeEach
     public void before() {
         componentManager = new ComponentManager(getComponentTypeFactory());
     }
--- gestalt-entity-system/src/test/java/org/terasology/gestalt/entitysystem/component/management/ComponentTypeIndexTest.java
+++ gestalt-entity-system/src/test/java/org/terasology/gestalt/entitysystem/component/management/ComponentTypeIndexTest.java
@@ -1,22 +1,10 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.gestalt.entitysystem.component.management;
 
-import org.junit.Test;
+import modules.test.components.Sample;
+import org.junit.jupiter.api.Test;
 import org.reflections.util.ClasspathHelper;
 import org.terasology.gestalt.assets.ResourceUrn;
 import org.terasology.gestalt.module.Module;
@@ -28,9 +16,7 @@ import org.terasology.gestalt.naming.Name;
 import java.util.Collections;
 import java.util.Optional;
 
-import modules.test.components.Sample;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  *
