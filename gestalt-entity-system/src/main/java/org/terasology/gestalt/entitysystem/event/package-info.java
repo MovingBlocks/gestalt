ef39b98b8d050927ee5f981200e11e2d565a0c60
Benjamin Amos <benjamin.amos11@gmail.com>
2024-07-27 11:38:36 +0100

    fix: expose gestalt-entity-system events to modules

--- gestalt-entity-system/src/main/java/org/terasology/gestalt/entitysystem/event/package-info.java
+++ gestalt-entity-system/src/main/java/org/terasology/gestalt/entitysystem/event/package-info.java
@@ -2,4 +2,7 @@
  * Support for sending events against entities. Events are delivered to handling methods, filtered by
  * the type of event and the components that an entity is composed of.
  */
+@API
 package org.terasology.gestalt.entitysystem.event;
+
+import org.terasology.context.annotation.API;
