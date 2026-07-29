31d3e833edf8a14d28632df20caed27396a9b7fa
Benjamin Amos <benjamin.amos11@gmail.com>
2026-07-29 20:22:05 +0100

    chore: update mockito to 5.23.0 to fix test failures

--- gestalt-module/src/test/java/org/terasology/gestalt/module/sandbox/APIScannerTest.java
+++ gestalt-module/src/test/java/org/terasology/gestalt/module/sandbox/APIScannerTest.java
@@ -21,7 +21,7 @@ import org.terasology.gestalt.di.index.UrlClassIndex;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
