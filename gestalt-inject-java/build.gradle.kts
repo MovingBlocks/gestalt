70c323f19973d6c3a1b7654db634d5c30e5e54ef
Benjamin Amos <benjamin.amos11@gmail.com>
2026-07-29 19:51:51 +0100

    build: fix gradle test failing on gestalt-inject-java due to there being no tests

--- gestalt-inject-java/build.gradle.kts
+++ gestalt-inject-java/build.gradle.kts
@@ -24,3 +24,8 @@ dependencies {
     implementation("javax.inject:javax.inject:1")
     implementation(project(":gestalt-inject"))
 }
+
+// https://docs.gradle.org/current/userguide/upgrading_major_version_9.html#test_task_fails_when_no_tests_are_discovered
+tasks.withType<AbstractTestTask>().configureEach {
+    failOnNoDiscoveredTests = false
+}
